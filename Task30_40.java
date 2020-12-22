06fb5ba487021d6319559f7dee692d35a56b8f84
Rejep Ataev <47426673+KoTeD0@users.noreply.github.com>
2020-12-23 01:57:24 +0300

    Commented tasks 30_40
    added comments to any task

--- Task30_40.java
+++ Task30_40.java
@@ -2,7 +2,9 @@ package com.company;
 
 import java.util.ArrayList;
 
-public class Task30_40 {
+/*Принимаем на вход текст и количество символов в строке, сплитим по пробелам, вставляем слово и пробел, и проверяем
+* хватает ли места на еще слово+пробел, если нет то смещаемся на след строку.*/
+public class Task30_40 { //task31
     public static String bessi(String s, int n){
         String[] arr = s.split(" ");
         StringBuilder s1 = new StringBuilder();
@@ -20,8 +22,9 @@ public class Task30_40 {
         }
         return s1.toString();
     }
-
-    public static String[] clasterSkobok(String string){
+/*сравниваем количество открытых и закрытых скобок, в момент его совпадения мы ставим пробел и дальше по пробелу делим
+ строку*/
+    public static String[] clasterSkobok(String string){ //task32
         String ans = "";
         int otkr = 0;
         int zakr = 0;
@@ -33,15 +36,15 @@ public class Task30_40 {
         }
         return ans.split(" ");
     }
-
-    public static String toCamelCase(String string){ //task 32
+/*проходимся по строке удаляя его и увеличиваем следующий*/
+    public static String toCamelCase(String string){ //task 33
         while (string.contains("_")){
             char temp = string.charAt(string.indexOf("_") + 1);
             string.replaceFirst("_"+temp, String.valueOf(Character.toUpperCase(temp)));
         }
         return string;
     }
-
+/*проходимся по строке и если есть буквы в верхнем регистре то уменьшаем их и двигаем подставляя нижнее подчеркивание*/
     public static String toSnakeCase(String string){ //task 33
         for (int i = 0; i < string.length() ; i++){
             if(Character.isUpperCase(string.charAt(i))){
@@ -51,7 +54,7 @@ public class Task30_40 {
         }
         return string;
     }
-
+/*если время окончания рабочего дня больше 17 то считаем сколько переработали и считаем по формулам*/
     public static String overTime(double[] a){ //task 34
         double money = 0;
         double overtime = 0;
@@ -59,7 +62,7 @@ public class Task30_40 {
         money = (a[1]-a[0]-overtime)*a[2] + (overtime * a[2]*a[3]);
         return "$" + String.format("%.2f",money);
     }
-
+/*принимаем вес и единицу измерения веса, а так же рост и единицу измерения роста, и считаем по предоставленным формулам*/
     public static String BMI(String mas, String length){ //task 35
         double m, l;
         String[] temp = mas.split(" ");
@@ -73,7 +76,7 @@ public class Task30_40 {
         else if (bmi >= 18.5 && bmi < 25)return String.format("%.1f",bmi) + " Normal weight";
         else return String.format("%.1f",bmi) + " Overweight";
     }
-
+/*путем хитрых мат. вычислений число умножаем позначно до тех пор пока это возможно и есть что умножать*/
     public static int bugger(int n){ //task 36
         int temp = 1, times = 0;
         while (n > 9) {
@@ -88,7 +91,8 @@ public class Task30_40 {
         }
         return times;
     }
-
+/*проверяем предыдущую букву и если она не совпадает с текущей i или = концу строки то прибавляем счетчик, и пока
+* не будет другая буква прибавляем счетчик, и потом "умножаем" слово на обороты счетчика*/
     public static String toStarShorthand(String string){ //task 37
         String ans = "";
         char temp = string.charAt(0);
@@ -106,42 +110,38 @@ public class Task30_40 {
         }
         return ans;
     }
-
+/*проверяем предпоследню букву в первом и втором предложении(с учетом точки) и сравниваем их*/
     public static boolean doesRhyme(String string1, String string2){ //task 38
-        char t1 = string1.charAt(string1.length() - 1)
-                , t2 = string2.charAt(string2.length() - 1);
+        char t1 = string1.charAt(string1.length() - 1) , t2 = string2.charAt(string2.length() - 1);
         if (!Character.isAlphabetic(string1.charAt(string1.length() - 1)))t1 = string1.charAt(string1.length() - 2);
         if (!Character.isAlphabetic(string2.charAt(string2.length() - 1)))t2 = string2.charAt(string2.length() - 2);
         return t1 == t2;
     }
-
+/*вводим числа, и по каждому числу пробегаемся, если какое то число встречается чаще constant то значение true
+* так же и со вторым, если оба boolean в конце == true то возвращем True*/
     public static boolean trouble(long a, long b){ //task 39
-        boolean[] n1 = new boolean[10];
-        boolean[] n2 = new boolean[10];
         for(int i = 0; i < 10; i++){
-            n1[i] = false;
-            n2[i] = false;
-            int t = 0;
-            long temp = a;
-            while (temp > 0){
-                if(temp % 10 == i)t++;
-                else t = 0;
-                if(t >= 3)n1[i] = true;
-                temp = temp / 10;
-            }
-
-            temp = b;
-            while (temp > 0){
-                if(temp % 10 == i)t++;
-                else t = 0;
-                if(t >= 2)n2[i] = true;
-                temp = temp / 10;
-            }
-            if(n1[i] == n2[i] && n1[i])return true;
+            boolean checkFirst = helper(a,i,3);
+            boolean checkSecond = helper(b,i,2);
+            if((checkFirst==checkSecond)&&checkFirst)
+                return true;
         }
         return false;
     }
 
+    private static boolean helper(long temp,int iter, int constant){
+        boolean[] digits = new boolean[10];
+        int t = 0;
+        while (temp > 0){
+            if(temp % 10 == iter) t++;
+            else t = 0;
+            if(t >= constant) digits[iter] = true;
+            temp /= 10;
+        }
+        return digits[iter];
+    }
+/*вводим строку и символ, пробегаемся по строке и за каждый уник символ прибавляем счетчик, если этот символ уже был до
+* этого то просто обрезаем начало.*/
     public static int countUniqueBooks(String string, char a){ //task 40
         int beginIndex = 0;
         int max = 0;
@@ -159,5 +159,7 @@ public class Task30_40 {
         }
         return max;
     }
+
+
 
 }
